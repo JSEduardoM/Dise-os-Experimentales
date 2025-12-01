ad45800992c1fcbd996f03c045d00e5d9fd3e520
JSEduardoM <julio20edu@gmail.com>
2025-11-30 23:45:29 -0500

    Feat: Update database

--- Analisis_Quinua.R
+++ Analisis_Quinua.R
@@ -16,3 +16,25 @@ df <- read_excel("Datos_Quinua.xlsx")
 head(df)
 
 str(df)
+
+# Exploración rápida
+df %>% group_by(Variedad, Fertilizante, Riego) %>%
+  summarise(n=n(), mean_y = mean(Rendimiento_kg), sd_y = sd(Rendimiento_kg))
+
+# Boxplot
+ggplot(df, aes(x=Fertilizante, y=Rendimiento_kg, fill=Riego)) +
+  geom_boxplot() + facet_wrap(~Variedad)
+
+# Modelo mixto (Bloque como efecto aleatorio)
+mod <- lmer(Rendimiento_kg ~ Variedad * Fertilizante * Riego + (1|Bloque), data=df)
+summary(mod)
+anova(mod)
+
+# Comprobación de supuestos
+plot(resid(mod) ~ fitted(mod))
+library(lmtest)
+bptest(lm(Rendimiento_kg ~ Variedad*Fertilizante*Riego + Bloque, data=df)) # Breusch-Pagan
+
+# Comparaciones post-hoc
+emm <- emmeans(mod, ~ Fertilizante | Variedad)
+pairs(emm, adjust="tukey")
